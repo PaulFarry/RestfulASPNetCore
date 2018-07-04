bfe5074484116d88f836f7675b2968042cc3c1c1
paulfarry <paulfarry@gmail.com>
2018-07-04 19:45:58 +1000

    Added List of books to new Author

--- RestfulASPNetCore.Web/Dtos/CreateAuthor.cs
+++ RestfulASPNetCore.Web/Dtos/CreateAuthor.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RestfulASPNetCore.Web.Dtos
 {
     public class CreateAuthor
     {
+        public CreateAuthor()
+        {
+            Books = new List<CreateBook>();
+        }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
 
         public DateTimeOffset DateOfBirth { get; set; }
         public string Genre { get; set; }
+
+        public ICollection<CreateBook> Books { get; set; }
     }
 }
